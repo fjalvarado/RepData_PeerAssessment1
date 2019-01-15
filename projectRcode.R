836b2fb0a31b60257979c11dda6410b89b8633a8
fjalvarado <francisco.alvarado@tec.mx>
2019-01-15 15:26:05 -0600

    Added steps per interval and date

--- projectRcode.R
+++ projectRcode.R
@@ -10,4 +10,28 @@ isweekday<-isWeekday(activitydata$date, wday=1:5)
 activitydata$wDay <- factor(isweekday, 
                    levels=c(FALSE, TRUE), labels=c('weekend', 'weekday'))
 
+## Unique days and intervals
+days<-unique(activitydata$date)
+intervals<-unique(activitydata$interval)
+
+
+## What is mean total number of steps taken per day?
+
+stepsperday<-activitydata %>% group_by(date) %>% summarize(steps=sum(steps,na.rm=T))
+
+## Histogram
+stepsday<-ggplot(stepsperday,aes(date,steps))
+stepsday+geom_col()
+
+## Mean and median
+summary(stepsperday$steps)
+
+## What is the average daily activity pattern?
+
+stepsperinterval<-activitydata %>% group_by(interval) %>% summarize(avgsteps=mean(steps,na.rm=T))
+
+## Time Series
+
+lineinterval<-ggplot(stepsperinterval,aes(interval,avgsteps))
+lineinterval+geom_line()
 
